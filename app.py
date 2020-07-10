f6e4171e5ae0cba4e892f6f15b33c5cf964dcc79
CodeWithEhtisham <shamikhan0347@gmail.com>
2020-07-10 16:17:04 +0500

    url update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from weatherData import weather
 from web_scraping import scrape
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for
 app=Flask(__name__)
 init=weather(weather.url,weather.api)
 obj=scrape(scrape.url1)
@@ -9,14 +9,23 @@ obj=scrape(scrape.url1)
 
 @app.route('/')
 def index():
-   print("index")
+   print("index func")
    weather=init.json_parser(init.get_request())
    product=obj.json_parser()
    data={"weather":weather,"product":product}
-   print(data["product"])
+   # print(data["product"])
    return render_template("index.html",data=data)
 
-
+@app.route('/trend')
+def trend():
+      print("trend func")
+    # show the form, it wasn't submitted
+      return render_template('trend.html')
+@app.route('/calender')
+def calender():
+      print("calender func")
+    # show the form, it wasn't submitted
+      return render_template('calender.html')
 
 
 if __name__ == "__main__":
